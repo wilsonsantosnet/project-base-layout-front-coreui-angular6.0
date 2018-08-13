b1a35b36560f7a7a3c6f85fe121e4490fbb0de1b
Wilson Santos <wilsonsantosnet@gmail.com>
2018-08-13 11:16:38 -0300

    ++ UnauthorizedComponent

--- src/app/app.custom.routing.ts
+++ src/app/app.custom.routing.ts
@@ -2,12 +2,13 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
-
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 const APP_ROUTES_CUSTOM: Routes = [
 
 	{ path: 'home', component: MainComponent },
-	{ path: 'login', component: LoginComponent }
+	{ path: 'login', component: LoginComponent },
+  { path: 'unauthorized', component: UnauthorizedComponent }
 
 ]
 
